b3e7ed99ec2d0bb4313ea1f865501302f9957d22
heyderpaez <heyderpaez@gmail.com>
2021-04-23 21:02:51 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -24,7 +24,7 @@ $app->get('/', function() use($app) {
 
 $app->get('/consulta', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return "Bienvenido a una consulta."
+  return "Bienvenido a una consulta.";
 });
 
 $app->run();
